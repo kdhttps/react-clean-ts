d1a25e1e728228df52c1aa2e2cd5ac46b7fa9759
kdhttps <kdhttps@gmail.com>
2023-02-23 15:29:50 +0530

    chore: add env

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>
+          {import.meta.env.REACT_APP_TITLE}
+        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly REACT_APP_TITLE: string
+}
